09f922d31f2a24243b43d94351ce82f0eaa79743
yench1995 <yench1995s@gmail.com>
2017-07-18 22:45:16 +0800

    first commit of myhttpd

--- Makefile
+++
@@ -1,7 +0,0 @@
-all: myHttpd 
-
-myHttpd: myHttpd.c
-			gcc -W -Wall -o myHttpd myHttpd.c -lpthread
-
-clean:
-		    rm myHttpd
---
+++ myHttpd/wrap_func.h
@@ -0,0 +1,6 @@
+#ifndef _WRAP_FUNC_H_
+#define _WRZP_FUNC_H_
+
+#include <sys/socket.h>
+#include <sys/types.h>
+#include <stdlib.h>
